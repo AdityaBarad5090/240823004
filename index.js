63cd6f8b156f9b572ca6ec2979d337719ba74d58
AdityaBarad5090 <adityasinhbarad@gmail.com>
2025-07-15 13:16:47 +0530

    nodejs practice

---
+++ index.js
@@ -0,0 +1,68 @@
+// console.log("Hello World")
+
+//prg 1 :Determine if a number is even or odd.
+
+// const a = 7;
+
+// if(a%2==0){
+//     console.log("even")
+// }
+// else{
+//     console.log("odd")
+// }
+
+
+//prg 2: Find the Maximum of Two Numbers
+
+// const a=5;
+// const b=7;
+
+// if(a>b){
+//     console.log("a is bigger")
+// }
+// else{
+//     console.log("b is bigger")
+// }
+
+
+//prg 3 : Check if a given year is a leap year
+
+// const year = 2020
+
+// if(year % 4 == 0){
+//     console.log("it is a leap year")
+// }
+// else{
+//     console.log("it is a normal year")
+// }
+
+// 4 : : Calculate the sum of the first n natural numbers.
+
+// const n = 5
+// let sum = 0
+
+// for(let i=1;i<=n;i++){
+//     sum+=i
+// }
+
+// console.log(`${sum} is sum of no`)
+
+// 5 : Calculate factorial of a number.
+
+// const num = 5
+// let fact=1
+
+// for(let i = 1;i<=num;i++){
+//     fact*=i
+// }
+// console.log(fact)
+
+//6 : print multipication table
+
+// const num = 6
+
+// for(let i=1;i<=10;i++){
+//     console.log(`${num} X ${i} = ${num*i}`)
+// }
+
+// 7 : Reverse the digits of a number
